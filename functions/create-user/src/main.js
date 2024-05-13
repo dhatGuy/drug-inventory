c5599eafe62dd531e4282466a8c2fad494f1bfe0
Joseph Odunsi <odunsiolakunbi@gmail.com>
2024-05-13 17:20:44 +0100

    [temp] test function

--- functions/create-user/src/main.js
+++ functions/create-user/src/main.js
@@ -1,33 +1,26 @@
-import { Client } from 'node-appwrite';
+import { Client, Databases } from "node-appwrite";
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+  const db = new Databases(client);
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+  // when a new user is created
+  if (req.method === "POST") {
+    const data = req.body;
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+    log.info("🚀 ~ data:", data);
+    // const user = await db.createDocument("drug-inventory", "user", ID.unique(), data);
+    // return res.json(user);
   }
 
-  // `res.json()` is a handy helper for sending JSON
   return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+    motto: "Build like a team of hundreds_",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
   });
 };
